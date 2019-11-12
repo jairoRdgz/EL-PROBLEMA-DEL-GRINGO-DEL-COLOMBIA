3d1a497be5b43806fc21977e80595f2992faea3f
jairoRdgz <jairitox2000@gmail.com>
2019-11-11 19:47:15 -0500

    Diagram and someMore

--- src/model_One/Graph.java
+++ src/model_One/Graph.java
@@ -1,4 +1,4 @@
-package model;
+package model_One;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/model_One/Vertice.java
+++ src/model_One/Vertice.java
@@ -1,4 +1,4 @@
-package model;
+package model_One;
 
 public class Vertice {
     private Nodo origen;
