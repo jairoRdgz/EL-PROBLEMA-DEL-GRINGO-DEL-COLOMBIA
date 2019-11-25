994f250c043c5d5aafb5d86359a2be4a0ec384d3
jairoRdgz <jairitox2000@gmail.com>
2019-11-25 13:02:33 -0500

    all posible graph

---
+++ src/model/Edge.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Edge<T> implements Comparable<Edge<T>>{
+
+	private double weight;
+
+	private Vertex<T> source;
+	private Vertex<T> destination;
+
+	public Edge(Vertex<T> source, Vertex<T> destination) {
+		this(source, destination, 1D);
+	}
+
+	public Edge(Vertex<T> source, Vertex<T> destination, double weight) {
+		this.source = source;
+		this.destination = destination;
+		this.weight = weight;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public Vertex<T> getSource() {
+		return source;
+	}
+
+	public Vertex<T> getDestination() {
+		return destination;
+	}
+
+	@Override
+	public int compareTo(Edge<T> o) {
+		return Double.compare(weight, o.weight);
+	}
+
+}
---
+++ src/model/UnionFind.java
@@ -0,0 +1,63 @@
+package model;
+
+// simple union-find based on int[] arrays
+// for  "parent" and "rank"
+// implements the "disjoint-set forests" described at
+// http://en.wikipedia.org/wiki/Disjoint-set_data_structure
+// which have almost constant "amortized" cost per operation
+// (actually O(inverse Ackermann))
+
+//Source: https://www.cs.waikato.ac.nz/~bernhard/317/source/graph/UnionFind.java
+
+import java.util.*;
+
+public class UnionFind {
+
+  private int[] parent;
+  private int[] rank;
+
+
+  public int find(int i) {
+
+    int p = parent[i];
+    if (i == p) {
+      return i;
+    }
+    return parent[i] = find(p);
+
+  }
+
+
+  public void union(int i, int j) {
+
+    int root1 = find(i);
+    int root2 = find(j);
+
+    if (root2 == root1) return;
+
+    if (rank[root1] > rank[root2]) {
+      parent[root2] = root1;
+    } else if (rank[root2] > rank[root1]) {
+      parent[root1] = root2;
+    } else {
+      parent[root2] = root1;
+      rank[root1]++;
+    }
+  }
+
+
+  public UnionFind(int max) {
+
+    parent = new int[max];
+    rank = new int[max];
+
+    for (int i = 0; i < max; i++) {
+      parent[i] = i;
+    }
+  }
+
+  
+  public String toString() {
+    return "<UnionFind\np " + Arrays.toString(parent) + "\nr " + Arrays.toString(rank) + "\n>";
+  }
+}
